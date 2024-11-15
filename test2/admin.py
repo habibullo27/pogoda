929bac120363a5bf2346636e32642a2329e3e69f
habibullo27 <khsanin2727@gmail.com>
2024-11-15 20:12:38 +0500

    pogoda

---
+++ test2/admin.py
@@ -0,0 +1,25 @@
+from itertools import product
+
+from django.contrib import admin
+from test2.models import CategoryProduct, Product, UserCart
+# Register your models here.
+@admin.register(CategoryProduct)
+class CategoryAdminModel(admin.ModelAdmin):
+    search_fields = ["category_name","id","created_at"]
+    list_filter = ["created_at"]
+    list_display = ["id","category_name" ,"created_at"]
+    ordering = ["-id"]
+
+@admin.register(Product)
+class ProductAdminModel(admin.ModelAdmin):
+    search_fields = ["product_name","id"]
+    list_filter = ["created_ad"]
+    list_display = ["id", "product_name", "product_cost"]
+    ordering = ["-id"]
+
+@admin.register(UserCart)
+class UserCartAdminModel(admin.ModelAdmin):
+    search_fields = ["user_id","id"]
+    list_filter = ["created_at"]
+    list_display = ["id", "user_id", "product"]
+    ordering = ["-id"]
