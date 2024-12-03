d8b0a2c11b01ffaa372ea4e17f94e9b1c031cdd8
habibullo27 <khsanin2727@gmail.com>
2024-12-03 14:50:52 +0500

    second

--- django57/settings.py
+++ django57/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+from django.conf.global_settings import DATABASES
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -74,10 +76,20 @@ WSGI_APPLICATION = 'django57.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+#DATABASES = {
+#   'default': {
+#      'ENGINE': 'django.db.backends.sqlite3',
+#     'NAME': BASE_DIR / 'db.sqlite3',
+#}
+#}
 DATABASES = {
-    'default': {
+    'default':{
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'sayt',
+        'USER': 'postgres',
+        'PORT': '5432',
+        'HOST' : 'localhost',
+        'PASSWORD' : '27122008'
     }
 }
 
